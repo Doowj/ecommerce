f00940be11221b1f7bd31262e21fdec89d85d208
Doowj <131784902+Doowj@users.noreply.github.com>
2025-09-17 11:28:48 +0800

    Update wis.sql

--- wis.sql
+++ wis.sql
@@ -17,12 +17,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `wis`
---
-CREATE DATABASE IF NOT EXISTS `wis` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `wis`;
-
 -- --------------------------------------------------------
 
 --
